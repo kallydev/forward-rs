6aea7d6283cfbde2084df36934b49e16e7c8df69
kekeimiku <2325373912@qq.com>
2020-04-29 19:21:06 +0800

    build: optimize for binary files

--- script/build_release.sh
+++ script/build_release.sh
@@ -12,6 +12,8 @@ for platform in "${!targets[@]}"; do
   target="${targets[$platform]}"
   rustup target add "$target"
   cargo build --target="$target" --release
+  strip -s ./target/"$target"/release/"$project"
+  upx -9 ./target/"$target"/release/"$project"
   7z a ./release/"$project"_"$platform".7z ./target/"$target"/release/"$project" README.md LICENSE
   sha1sum ./release/"$project"_"$platform".7z > ./release/"$project"_"$platform".7z.sha1
 done
